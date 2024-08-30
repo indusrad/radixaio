13f3ca207d2f259d67ba0f3ee610a637ddb3b28c
srsirad <ssamineni@indusrad.com>
2024-08-30 09:18:43 -0400

    Under Construction

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.indusrad.radixaio',
+  appName: 'radixaio',
+  webDir: 'dist'
+};
+
+export default config;
